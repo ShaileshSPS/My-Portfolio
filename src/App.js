93cc299cd724804a6354e67a72c1985af74b9c7e
Shailesh <shailesh.shanbhags@gmail.com>
2024-02-29 20:32:17 +0530

    modified App.js file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import About from './components/About';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <Switch>
+      <div>
+        <div className="header">
+          <h1>My Portfolio</h1>
+        </div>
+        <div className="navbar">
+          <Link to="/">About</Link>
+          <Link to="/projects">Projects</Link>
+          <Link to="/skills">Skills</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
         <Route path="/" exact component={About} />
         <Route path="/projects" component={Projects} />
         <Route path="/skills" component={Skills} />
         <Route path="/contact" component={Contact} />
-      </Switch>
+      </div>
     </Router>
   );
-}
+};
 
 export default App;
